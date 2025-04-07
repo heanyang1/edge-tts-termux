df22734d3cde0cf1387c76c9eb8b652bc1dab884
heanyang1 <e1124486@u.nus.edu>
2025-04-07 16:10:34 +0800

    pause and unpause audio

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 An [edge-tts](https://github.com/rany2/edge-tts) frontend on Android using [Termux:GUI Python bindings](https://github.com/tareksander/termux-gui-python-bindings).
 
-## Usage
+## Install and usage
 
 Install [Termux:GUI](https://github.com/termux/termux-gui) and dependencies:
 ```sh
@@ -9,6 +9,17 @@ pip install edge-tts termuxgui
 ```
 
 Then run `python tts.py` on your Termux session.
+
+GUI usage:
+1. Enter the text you want to transform into speech and a file name
+2. Set a voice (use the spinbox on the left to filter voices with certain language)
+3. Press `request` to send a TTS request and download it as an mp3 audio file
+4. Press `play` to play the downloaded audio
+5. Uncheck the checkbox if you want to keep the downloaded file
+
+## Screenshot
+
+![](screenshot.jpg)
 
 ## License
 
--- tts.py
+++ tts.py
@@ -1,17 +1,17 @@
 """
-   Copyright 2025 heanyang1
+Copyright 2025 heanyang1
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License. 
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 """
 
 import os
@@ -20,18 +20,71 @@ import sys
 import edge_tts
 import asyncio
 import subprocess
+import json
+import socket
+from enum import Enum
 
 voices_and_languages = asyncio.run(edge_tts.list_voices())
-
 all_languages = "All languages"
 languages = [all_languages] + list(
     sorted(set([voice.get("Locale") for voice in voices_and_languages]))
 )
-language = languages[0]
-
 choose_voice = "Choose a voice"
 voices = [choose_voice] + [voice.get("ShortName") for voice in voices_and_languages]
+
+socket_in, socket_out = socket.socketpair(socket.AF_UNIX, socket.SOCK_STREAM, 0)
+mpv = None
+
+class State(Enum):
+    START = 0
+    DOWNLOADED = 1
+    PLAYING = 2
+    STOPPED = 3
+
+
+state = State.START
+
+# Default values
+language = all_languages
 voice = choose_voice
+remove_file = True
+text = "Enter text here"
+filename = "Filename.mp3"
+
+
+def request_tts(c, text, name, rate, pitch, volume):
+    if voice == choose_voice:
+        c.toast("Please choose a voice")
+    elif text == "":
+        c.toast("Please enter some text")
+    elif name == "":
+        c.toast("Please enter output filename")
+    else:
+        c.toast("Sending request...")
+        communicate = edge_tts.Communicate(
+            text,
+            voice,
+            rate=rate,
+            pitch=pitch,
+            volume=volume,
+        )
+        communicate.save_sync(name)
+
+
+def send_command(s: socket.socket, command: list):
+    s.sendall((json.dumps({"command": command}) + "\n").encode())
+    # mpv returns a response after the command is executed
+    # we receive the response and discard it to make sure the command was executed
+    s.recv(1024)
+
+
+def quit_mpv():
+    global mpv
+    if state in [State.PLAYING, State.STOPPED]:
+        assert mpv is not None
+        mpv.terminate()
+        mpv = None
+
 
 with tg.Connection() as c:
     a = tg.Activity(c)
@@ -43,8 +96,8 @@ with tg.Connection() as c:
     title.settextsize(30)
     title.setmargin(5)
 
-    et1 = tg.EditText(a, "Enter text here", layout, inputtype="textMultiLine")
-    et2 = tg.EditText(a, "Filename.mp3", layout)
+    et1 = tg.EditText(a, text, layout, inputtype="textMultiLine")
+    et2 = tg.EditText(a, filename, layout)
 
     spinners = tg.LinearLayout(a, layout, vertical=False)
     spinners.setheight(tg.View.WRAP_CONTENT)
@@ -55,56 +108,63 @@ with tg.Connection() as c:
     voice_spinner = tg.Spinner(a, spinners)
     voice_spinner.setlist(voices)
 
+    remove_file_checkbox = tg.Checkbox(a, "Remove file on exit", layout, remove_file)
+    remove_file_checkbox.setheight(tg.View.WRAP_CONTENT)
+    remove_file_checkbox.setlinearlayoutparams(0)
+
     option = tg.LinearLayout(a, layout, vertical=False)
     option.setheight(tg.View.WRAP_CONTENT)
     option.setlinearlayoutparams(0)
 
-    tg.TextView(a, "Rate: ", option).settextsize(19)
+    tg.TextView(a, "Rate: ", option)
     rate = tg.EditText(a, "+0%", option)
 
-    tg.TextView(a, "Pitch: ", option).settextsize(19)
+    tg.TextView(a, "Pitch: ", option)
     pitch = tg.EditText(a, "+0Hz", option)
 
-    tg.TextView(a, "Volume: ", option).settextsize(19)
+    tg.TextView(a, "Volume: ", option)
     volume = tg.EditText(a, "+0%", option)
 
     buttons = tg.LinearLayout(a, layout, vertical=False)
     buttons.setheight(tg.View.WRAP_CONTENT)
     buttons.setlinearlayoutparams(0)
 
-    save = tg.Button(a, "save", buttons)
+    request = tg.Button(a, "request", buttons)
     play = tg.Button(a, "play", buttons)
     exit_ = tg.Button(a, "exit", buttons)
 
     for ev in c.events():
         if ev.type == tg.Event.destroy and ev.value["finishing"]:
             sys.exit()
-        if ev.type == tg.Event.click and ev.value["id"] in [save, play]:
+        elif ev.type == tg.Event.click and ev.value["id"] == request:
+            quit_mpv()
             text = et1.gettext()
-            name = et2.gettext()
-            if voice == choose_voice:
-                c.toast("Please choose a voice")
-            elif text == "":
-                c.toast("Please enter some text")
-            elif name == "":
-                c.toast("Please enter output filename")
-            else:
-                communicate = edge_tts.Communicate(
-                    text,
-                    voice,
-                    rate=rate.gettext(),
-                    pitch=pitch.gettext(),
-                    volume=volume.gettext(),
+            filename = et2.gettext()
+            request_tts(
+                c, text, filename, rate.gettext(), pitch.gettext(), volume.gettext()
+            )
+            c.toast("The audio file is ready")
+            state = State.DOWNLOADED
+        elif ev.type == tg.Event.click and ev.value["id"] == play:
+            if state == State.START:
+                c.toast("Press 'request' first")
+            elif state == State.DOWNLOADED:
+                assert mpv is None
+                mpv = subprocess.Popen(
+                    ["mpv", filename, f"--input-ipc-client=fd://{socket_in.fileno()}"],
+                    pass_fds=[socket_in.fileno()],
                 )
-                c.toast("Sending request...")
-                communicate.save_sync(name)
-                if ev.value["id"] == save:
-                    a.finish()
-                elif ev.value["id"] == play:
-                    with subprocess.Popen(["mpv", name]) as process:
-                        process.communicate()
-                    os.unlink(name)
-        if ev.type == tg.Event.itemselected and ev.value["id"] == language_spinner:
+                play.settext("pause")
+                state = State.PLAYING
+            elif state == State.PLAYING:
+                send_command(socket_out, ["keypress", "p"])
+                play.settext("play")
+                state = State.STOPPED
+            elif state == State.STOPPED:
+                send_command(socket_out, ["keypress", "p"])
+                play.settext("pause")
+                state = State.PLAYING
+        elif ev.type == tg.Event.itemselected and ev.value["id"] == language_spinner:
             language = ev.value["selected"]
             voices = [choose_voice] + [
                 voice.get("ShortName")
@@ -113,7 +173,16 @@ with tg.Connection() as c:
             ]
             voice = choose_voice
             voice_spinner.setlist(voices)
-        if ev.type == tg.Event.itemselected and ev.value["id"] == voice_spinner:
+        elif ev.type == tg.Event.itemselected and ev.value["id"] == voice_spinner:
             voice = ev.value["selected"]
-        if ev.type == tg.Event.click and ev.value["id"] == exit_:
-            a.finish()
+        elif ev.type == tg.Event.click and ev.value["id"] == remove_file_checkbox:
+            remove_file = ev.value["set"]
+        elif ev.type == tg.Event.click and ev.value["id"] == exit_:
+            break
+
+    quit_mpv()
+    if remove_file and os.path.exists(filename):
+        os.unlink(filename)
+    socket_in.close()
+    socket_out.close()
+    a.finish()
